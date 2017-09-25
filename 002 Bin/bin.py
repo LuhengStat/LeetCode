5ee7d203579730b786b2fd2b59ae9778d51c67bd
LuhengStat <30485426+LuhengStat@users.noreply.github.com>
2017-09-25 14:07:52 +0800

    accepted

--- 002 Bin/bin.py
+++ 002 Bin/bin.py
@@ -1,15 +1,38 @@
-import math
-def BitCal(x):
-    flag = 1
-    out = []
-    while flag:
-        if x == 0:
-            a = 0
-            flag = 0
-        else:
-            a = int(math.log(x) / math.log(2))
-            x = x - 2 ** a
-            out.append(a)
-    return out
+class Solution(object):
+    import math
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        import math
+        def BitCal(x):
+            flag = 1
+            out = []
+            while flag:
+                if x == 0:
+                    a = 0
+                    flag = 0
+                else:
+                    a = int(math.log(x) / math.log(2))
+                    x = x - 2 ** a
+                    out.append(a)
+            return out
 
-print BitCal(420)
+        xbit = BitCal(x)
+        ybit = BitCal(y)
+
+        Union = xbit[:]
+        for i in ybit:
+            if i not in xbit:
+                Union.append(i)
+
+        InterSec = []
+        for i in xbit:
+            if i in ybit:
+                InterSec.append(i)
+
+        return len(Union) - len(InterSec)
+
+    print hammingDistance(1, 1, 4)
